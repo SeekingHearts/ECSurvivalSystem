2dd822684e0f2f79e313ced6308daed3bea2e2d2
Aaron <unconfigured@null.spigotmc.org>
2018-09-14 21:03:50 +0200

    Fixed String Error

--- [EC] SurvivalSystem/src/me/aaron/survivalsystem/utils/ItemUtils.java
+++ [EC] SurvivalSystem/src/me/aaron/survivalsystem/utils/ItemUtils.java
@@ -11,78 +11,102 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemUtils {
-	
+
 	private static ItemUtils instance;
 	Material mat;
 	ItemStack itm;
 	ItemMeta meta;
-	
-	public ItemUtils() {}
-	
+
+	public ItemUtils() {
+	}
+
 	public ItemUtils(Material mat) {
 		mat = mat;
 		itm = new ItemStack(mat);
 		meta = itm.getItemMeta();
 	}
-	
+
 	public ItemUtils(ItemStack itm) {
 		itm = itm;
 		meta = itm.getItemMeta();
 	}
-	
+
 	public ItemUtils setDisplay(String name) {
 		meta.setDisplayName(name);
 		return this;
 	}
-	
+
 	public ItemUtils setLore(List<String> lores) {
 		meta.setLore(lores);
 		return this;
 	}
-	
+
 	public ItemUtils setData(short dur) {
 		itm.setDurability(dur);
 		return this;
 	}
-	
+
 	public ItemUtils setAmount(int amount) {
 		itm.setAmount(amount);
 		return this;
 	}
-	
+
 	public ItemUtils setEnchantment(Enchantment enchantment, int amount) {
 		itm.addUnsafeEnchantment(enchantment, amount);
 		return this;
 	}
-	
+
 	public ItemUtils setAttributs(ItemFlag flag) {
 		meta.addItemFlags(flag);
 		return this;
 	}
-	
+
 	public ItemStack build() {
 		itm.setItemMeta(meta);
 		return itm;
 	}
-	
+
 	public static ItemUtils getInstance() {
 		return instance;
 	}
-	
+
 	public static ItemStack getItem(Material mat, String name, List<String> list, int dmg, int amount) {
-		
+
 		ItemStack itm = new ItemStack(mat, amount, (short) dmg);
 		ItemMeta meta = itm.getItemMeta();
-		
+
 		if (list != null) {
-				meta.setLore(list);
+			meta.setLore(list);
 		}
 		meta.setDisplayName(name);
 		itm.setItemMeta(meta);
-		
+
 		return itm;
 	}
-	
+
+	public static ItemStack getItem(Material mat, String name, String list, int dmg, int amount) {
+
+		ItemStack itm = new ItemStack(mat, amount, (short) dmg);
+		ItemMeta meta = itm.getItemMeta();
+
+		if (list != null) {
+			if (list.contains("\n")) {
+				String[] lore_ = list.split("\n");
+				List<String> lore = new ArrayList<>();
+				for (String str : lore_) {
+					lore.add(str);
+				}
+				meta.setLore(lore);
+			} else {
+				meta.setLore(Arrays.asList(list));
+			}
+		}
+		meta.setDisplayName(name);
+		itm.setItemMeta(meta);
+
+		return itm;
+	}
+
 	static {
 		instance = new ItemUtils();
 	}
