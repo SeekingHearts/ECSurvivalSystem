cd5dff1a1cb55a7b038009e8b52d0b35bc159075
Terra <Terra@DESKTOP-RNFJDQ0>
2018-10-03 15:25:50 +0200

    SetupInventory: +Items, Interface Update

--- [EC] SurvivalSystem/src/me/aaron/survivalsystem/listeners/SetupInventory.java
+++ [EC] SurvivalSystem/src/me/aaron/survivalsystem/listeners/SetupInventory.java
@@ -39,6 +39,14 @@ public class SetupInventory implements Listener {
 			Arrays.asList(ChatColor.GRAY + "Setze PVP freie Zone, PVE erlaubt"), 0, 1);
 	ItemStack setSaveZone = ItemUtils.getItem(Material.EMERALD_BLOCK, "§aSetze eine sichere Zone",
 			Arrays.asList(ChatColor.GRAY + "Setze eine Zone, in welcher man kein Schaden bekommen kann"), 0, 1);
+	ItemStack setprotectedZone = ItemUtils.getItem(Material.DIAMOND_BLOCK, "§aSetze eine geschützte Zone",
+			Arrays.asList(ChatColor.GRAY + "Setze eine Zone, in der man nicht abbauen und kein PVP erlaubt ist"), 0, 1);
+	ItemStack setprotectedPVPZone = ItemUtils.getItem(Material.DIAMOND_BLOCK, "§aSetze eine geschützte PVP Zone",
+			Arrays.asList(ChatColor.GRAY + "Setze eine Zone, in der man nicht abbauen kann, aber PVP erlaubt ist"), 0,
+			1);
+	ItemStack setFarmZone = ItemUtils.getItem(Material.BIRCH_WOOD, "§aSetze eine farm Zone",
+			Arrays.asList(ChatColor.GRAY + "Setze eine Zone, in der man nicht abbauen kann, aber PVP erlaubt ist"), 0,
+			1);
 
 	@EventHandler
 	public void onCompass(PlayerInteractEvent e) {
@@ -126,7 +134,6 @@ public class SetupInventory implements Listener {
 					rg.setFlag(Flags.DAMAGE_ANIMALS, State.DENY);
 					rg.setFlag(Flags.DESTROY_VEHICLE, State.DENY);
 					rg.setFlag(Flags.ENDERPEARL, State.DENY);
-					rg.setFlag(Flags.FALL_DAMAGE, State.DENY);
 					rg.setFlag(Flags.WITHER_DAMAGE, State.DENY);
 					rg.setFlag(Flags.MOB_DAMAGE, State.DENY);
 					rg.setFlag(Flags.GRASS_SPREAD, State.DENY);
@@ -150,7 +157,6 @@ public class SetupInventory implements Listener {
 							+ ChatColor.GREEN + "' wurde §berfolgreich " + ChatColor.GREEN + "erstellt!");
 				}
 			} else if (e.getCurrentItem() == setNoPVPZone) {
-				p.sendMessage("Tool setNoPVPZone");
 				World w = new BukkitWorld(
 						Bukkit.getServer().getWorld(Main.getInstance().getConfig().getString("locations.spawn.world")));
 				double sx = Main.getInstance().getConfig().getDouble("locations.spawn.x");
@@ -173,10 +179,10 @@ public class SetupInventory implements Listener {
 					rg.setMembers(dm);
 					rg.setFlag(Flags.PVP, State.DENY);
 					rg.setFlag(Flags.TNT, State.DENY);
+					rg.setFlag(Flags.CREEPER_EXPLOSION, State.DENY);
 				}
-			}
-			else if (e.getCurrentItem() == setNoPVPZone) {
-				p.sendMessage("Tool setNoPVPZone");
+				p.sendMessage(ChatColor.GREEN + "PVP sichere Zone gesetzt!");
+			} else if (e.getCurrentItem() == setSaveZone) {
 				World w = new BukkitWorld(
 						Bukkit.getServer().getWorld(Main.getInstance().getConfig().getString("locations.spawn.world")));
 				double sx = Main.getInstance().getConfig().getDouble("locations.spawn.x");
@@ -215,10 +221,142 @@ public class SetupInventory implements Listener {
 					rg.setFlag(Flags.TNT, State.DENY);
 					rg.setFlag(Flags.FALL_DAMAGE, State.DENY);
 				}
+				p.sendMessage(ChatColor.GREEN + "sichere Zone gesetzt!");
+			} else if (e.getCurrentItem() == setprotectedZone) {
+				World w = new BukkitWorld(
+						Bukkit.getServer().getWorld(Main.getInstance().getConfig().getString("locations.spawn.world")));
+				double sx = Main.getInstance().getConfig().getDouble("locations.spawn.x");
+				double sy = Main.getInstance().getConfig().getDouble("locations.spawn.y");
+				double sz = Main.getInstance().getConfig().getDouble("locations.spawn.z");
+				float syaw = (float) Main.getInstance().getConfig().getDouble("locations.spawn.yaw");
+				float spitch = (float) Main.getInstance().getConfig().getDouble("locations.spawn.pitch");
+				ProtectedCuboidRegion rg = new ProtectedCuboidRegion("spawnarea-" + w.getName(),
+						new BlockVector(sx + 200, w.getMaxY() + 1, sy + 200),
+						new BlockVector(sx - 200, -(w.getMaxY() + 1), sy - 200));
+				DefaultDomain dd = new DefaultDomain();
+				DefaultDomain dm = new DefaultDomain();
+				for (Player ap : Bukkit.getServer().getOnlinePlayers()) {
+					if (ap.hasPermission("survivalsystem.regions.spawn")) {
+						dd.addPlayer(Main.getWorldGuard().wrapPlayer(ap));
+					} else {
+						dm.addPlayer(Main.getWorldGuard().wrapPlayer(ap));
+					}
+					rg.setOwners(dd);
+					rg.setMembers(dm);
+					rg.setFlag(Flags.PVP, State.DENY);
+					rg.setFlag(Flags.BLOCK_BREAK, State.DENY);
+					rg.setFlag(Flags.BLOCK_PLACE, State.DENY);
+					rg.setFlag(Flags.BUILD, State.DENY);
+					rg.setFlag(Flags.ITEM_DROP, State.DENY);
+					rg.setFlag(Flags.CREEPER_EXPLOSION, State.DENY);
+					rg.setFlag(Flags.DAMAGE_ANIMALS, State.DENY);
+					rg.setFlag(Flags.DESTROY_VEHICLE, State.DENY);
+					rg.setFlag(Flags.ENDERPEARL, State.DENY);
+					rg.setFlag(Flags.WITHER_DAMAGE, State.DENY);
+					rg.setFlag(Flags.MOB_DAMAGE, State.DENY);
+					rg.setFlag(Flags.GRASS_SPREAD, State.DENY);
+					rg.setFlag(Flags.ICE_MELT, State.DENY);
+					rg.setFlag(Flags.LAVA_FIRE, State.DENY);
+					rg.setFlag(Flags.FIRE_SPREAD, State.DENY);
+					rg.setFlag(Flags.MOB_SPAWNING, State.DENY);
+					rg.setFlag(Flags.MYCELIUM_SPREAD, State.DENY);
+					rg.setFlag(Flags.VINE_GROWTH, State.DENY);
+					rg.setFlag(Flags.TNT, State.DENY);
+					rg.setFlag(Flags.SNOW_MELT, State.DENY);
+					rg.setFlag(Flags.FALL_DAMAGE, State.DENY);
+				}
+				p.sendMessage(ChatColor.GREEN + "gesicherte Zone gesetzt!");
+
+			} else if (e.getCurrentItem() == setprotectedPVPZone)
+
+			{
+				World w = new BukkitWorld(
+						Bukkit.getServer().getWorld(Main.getInstance().getConfig().getString("locations.spawn.world")));
+				double sx = Main.getInstance().getConfig().getDouble("locations.spawn.x");
+				double sy = Main.getInstance().getConfig().getDouble("locations.spawn.y");
+				double sz = Main.getInstance().getConfig().getDouble("locations.spawn.z");
+				float syaw = (float) Main.getInstance().getConfig().getDouble("locations.spawn.yaw");
+				float spitch = (float) Main.getInstance().getConfig().getDouble("locations.spawn.pitch");
+				ProtectedCuboidRegion rg = new ProtectedCuboidRegion("spawnarea-" + w.getName(),
+						new BlockVector(sx + 200, w.getMaxY() + 1, sy + 200),
+						new BlockVector(sx - 200, -(w.getMaxY() + 1), sy - 200));
+				DefaultDomain dd = new DefaultDomain();
+				DefaultDomain dm = new DefaultDomain();
+				for (Player ap : Bukkit.getServer().getOnlinePlayers()) {
+					if (ap.hasPermission("survivalsystem.regions.spawn")) {
+						dd.addPlayer(Main.getWorldGuard().wrapPlayer(ap));
+					} else {
+						dm.addPlayer(Main.getWorldGuard().wrapPlayer(ap));
+					}
+					rg.setOwners(dd);
+					rg.setMembers(dm);
+					rg.setFlag(Flags.PVP, State.ALLOW);
+					rg.setFlag(Flags.BLOCK_BREAK, State.DENY);
+					rg.setFlag(Flags.BLOCK_PLACE, State.DENY);
+					rg.setFlag(Flags.BUILD, State.DENY);
+					rg.setFlag(Flags.ITEM_DROP, State.ALLOW);
+					rg.setFlag(Flags.CREEPER_EXPLOSION, State.DENY);
+					rg.setFlag(Flags.DAMAGE_ANIMALS, State.ALLOW);
+					rg.setFlag(Flags.DESTROY_VEHICLE, State.DENY);
+					rg.setFlag(Flags.ENDERPEARL, State.ALLOW);
+					rg.setFlag(Flags.WITHER_DAMAGE, State.DENY);
+					rg.setFlag(Flags.MOB_DAMAGE, State.ALLOW);
+					rg.setFlag(Flags.GRASS_SPREAD, State.DENY);
+					rg.setFlag(Flags.ICE_MELT, State.DENY);
+					rg.setFlag(Flags.LAVA_FIRE, State.ALLOW);
+					rg.setFlag(Flags.FIRE_SPREAD, State.DENY);
+					rg.setFlag(Flags.MOB_SPAWNING, State.DENY);
+					rg.setFlag(Flags.MYCELIUM_SPREAD, State.DENY);
+					rg.setFlag(Flags.VINE_GROWTH, State.DENY);
+					rg.setFlag(Flags.SNOW_MELT, State.DENY);
+					rg.setFlag(Flags.FALL_DAMAGE, State.ALLOW);
+				}
+				p.sendMessage(ChatColor.GREEN + "geschützte PVP Zone gesetzt!");
+			} else if (e.getCurrentItem() == setFarmZone) {
+				World w = new BukkitWorld(
+						Bukkit.getServer().getWorld(Main.getInstance().getConfig().getString("locations.spawn.world")));
+				double sx = Main.getInstance().getConfig().getDouble("locations.spawn.x");
+				double sy = Main.getInstance().getConfig().getDouble("locations.spawn.y");
+				double sz = Main.getInstance().getConfig().getDouble("locations.spawn.z");
+				float syaw = (float) Main.getInstance().getConfig().getDouble("locations.spawn.yaw");
+				float spitch = (float) Main.getInstance().getConfig().getDouble("locations.spawn.pitch");
+				ProtectedCuboidRegion rg = new ProtectedCuboidRegion("spawnarea-" + w.getName(),
+						new BlockVector(sx + 200, w.getMaxY() + 1, sy + 200),
+						new BlockVector(sx - 200, -(w.getMaxY() + 1), sy - 200));
+				DefaultDomain dd = new DefaultDomain();
+				DefaultDomain dm = new DefaultDomain();
+				for (Player ap : Bukkit.getServer().getOnlinePlayers()) {
+					if (ap.hasPermission("survivalsystem.regions.spawn")) {
+						dd.addPlayer(Main.getWorldGuard().wrapPlayer(ap));
+					} else {
+						dm.addPlayer(Main.getWorldGuard().wrapPlayer(ap));
+					}
+					dd.addPlayer(Main.getWorldGuard().wrapPlayer(p));
+					rg.setOwners(dd);
+					rg.setMembers(dm);
+					rg.setFlag(Flags.PVP, State.DENY);
+					rg.setFlag(Flags.ITEM_DROP, State.DENY);
+					rg.setFlag(Flags.CREEPER_EXPLOSION, State.DENY);
+					rg.setFlag(Flags.DAMAGE_ANIMALS, State.DENY);
+					rg.setFlag(Flags.DESTROY_VEHICLE, State.DENY);
+					rg.setFlag(Flags.FALL_DAMAGE, State.DENY);
+					rg.setFlag(Flags.WITHER_DAMAGE, State.DENY);
+					rg.setFlag(Flags.MOB_DAMAGE, State.DENY);
+					rg.setFlag(Flags.ICE_MELT, State.DENY);
+					rg.setFlag(Flags.ITEM_PICKUP, State.DENY);
+					rg.setFlag(Flags.LAVA_FIRE, State.DENY);
+					rg.setFlag(Flags.LIGHTNING, State.DENY);
+					rg.setFlag(Flags.MOB_SPAWNING, State.DENY);
+					rg.setFlag(Flags.MYCELIUM_SPREAD, State.DENY);
+					rg.setFlag(Flags.TNT, State.DENY);
+					rg.setFlag(Flags.FALL_DAMAGE, State.DENY);
+				}
+				p.sendMessage(ChatColor.GREEN + "farm Zone gesetzt!");
 			}
 			e.setCancelled(true);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
+
 }
